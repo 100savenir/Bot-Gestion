3b2c188eea404bb5f03b37dc91d8b2e0ad088d8b
100savenir <dexterity.js@proton.me>
2024-11-25 21:58:03 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    clients: {
+        token: '', // token
+        prefix: '+', // prefix
+        embedColor: '#ff0000', // EmbedColor
+        name: '', // EmbedFooter
+        logo: '', // Embed footer image
+        buyer: '' // your ID
+    }
+}
